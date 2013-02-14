d7c5efce1b05b4cb6bdbae63865c72944dd86c98
Mark Norman Francis <norm@cackhanded.net>
2013-02-14 10:06:17 +0000

    Expose location, licence and interaction data.
    @pbadenski
    @norm

--- read-api/app/controllers/application_controller.rb
+++ read-api/app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def licensing
+    client = Mongo::Connection.new
+    db = client['performance_platform']
+    coll = db['licensing']
+    results = coll.find.to_a
+    results = results.map { |each| each.delete("_id"); each }
+    
+    render :json => results.to_json
+  end
 end
