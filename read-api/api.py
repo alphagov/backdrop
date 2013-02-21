378544d3eb346fa878891f5a9ecefbca82cd5d99
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2013-02-21 17:01:20 +0000

    Add start_at attribute to read-api

--- read-api/api.py
+++ read-api/api.py
@@ -1,5 +1,8 @@
-from flask import Flask, jsonify
+from dateutil import parser
+
+from flask import Flask, jsonify, request
 from pymongo import MongoClient
+import pytz
 
 
 # Configuration
@@ -12,14 +15,38 @@ app.config.from_object(__name__)
 mongo = MongoClient('localhost', 27017)
 
 
-def collection(bucket):
+def open_bucket_collection(bucket):
     return mongo[app.config['DATABASE_NAME']][bucket]
 
 
 @app.route('/<bucket>', methods=['GET'])
 def query(bucket):
-    return jsonify(data=list(collection(bucket).find()))
+    collection = open_bucket_collection(bucket)
 
+    query = {}
+
+    if "start_at" in request.args:
+        query["_timestamp"] = {
+            "$gte": time_string_to_utc_datetime(request.args['start_at'])
+        }
+
+    response_data = [
+        jsonify_document(document) for document in collection.find(query)
+    ]
+
+    return jsonify(data=response_data)
+
+
+def time_string_to_utc_datetime(time_string):
+    time = parser.parse(time_string)
+    return time.astimezone(pytz.utc)
+
+
+def jsonify_document(document):
+    if '_timestamp' in document:
+        document['_timestamp'] = document['_timestamp'].isoformat()
+
+    return document
 
 if __name__ == '__main__':
     app.debug = True
--- read-api/tests/test_api.py
+++ read-api/tests/test_api.py
@@ -1,8 +1,9 @@
 import os
 import unittest
+from dateutil import parser
 
-from pymongo import MongoClient
 from flask import json
+import pytz
 
 import api
 
@@ -14,6 +15,8 @@ def load_fixture(collection_name, fixture_name):
     fixture_path = os.path.join(root_path, 'test', 'data', fixture_name)
     with open(fixture_path) as fixture:
         for document in json.load(fixture):
+            if "_timestamp" in document:
+                document["_timestamp"] = parser.parse(document["_timestamp"]).astimezone(pytz.utc)
             api.mongo[DATABASE_NAME][collection_name].save(document)
 
 
@@ -23,12 +26,33 @@ class MyFlaskTestCase(unittest.TestCase):
         load_fixture('licencing', "licence.json")
         self.app = api.app.test_client()
 
-    def tearDown(self):
-        api.mongo[DATABASE_NAME]['licencing'].drop()
+#    def tearDown(self):
+#        api.mongo[DATABASE_NAME]['licencing'].drop()
 
     def test_that_with_no_arguments_all_documents_are_returned(self):
         response = self.app.get('/licencing')
 
+        self.assertEqual(response.status_code, 200)
+
         response_data = json.loads(response.data)
 
         self.assertEqual(len(response_data['data']), 3)
+
+    def test_that_events_later_than_start_at_are_returned(self):
+        response = self.app.get('/licencing?start_at=2012-12-12T01:01:02%2B00:00')
+
+        self.assertEqual(response.status_code, 200)
+
+        response_data = json.loads(response.data)
+
+        self.assertEqual(len(response_data['data']), 1)
+
+
+    def test_that_events_equal_to_or_later_than_start_at_are_returned(self):
+        response = self.app.get('/licencing?start_at=2012-12-13T01:01:01%2B00:00')
+
+        self.assertEqual(response.status_code, 200)
+
+        response_data = json.loads(response.data)
+
+        self.assertEqual(len(response_data['data']), 1)
