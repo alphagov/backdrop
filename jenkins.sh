dff778c94f962854881a51b7e50d95950b9ec4c6
Gareth Rogers <grogers@thoughtworks.com>
2013-02-22 17:39:25 +0000

    Updating jenkins.sh to run the tests.
    @robyoung
    @gtrogers

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
-echo 'this does not currently run any tests'
+set -e
+
+if [ -e venv ]; then
+  rm -r venv
+fi
+virtualenv venv
+source ./venv/bin/activate
+
+pip install -r requirements.txt
+pip install -r requirements_for_tests.txt
+
+PYTHONPATH=./tests/read behave ./tests/read/features
+RESULT=$?
+
+if [ $RESULT -ne 0 ]; then
+  echo 'user tests failed'
+  exit 1
+fi
+
+nosetests
+RESULT=$?
+
+if [ $RESULT -ne 0 ]; then
+  echo 'developer tests failed'
+  exit 2
+fi
 
 exit 0
