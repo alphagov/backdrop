5d83d5612953a82e0e060ef7a8313334c21e1327
Max Fliri <mfliri@thoughtworks.com>
2013-02-27 17:32:15 +0000

    Factory method for test client
    @maxfliri

--- features/environment.py
+++ features/environment.py
@@ -13,15 +13,7 @@ os.environ["FLASK_ENV"] = "test"
 
 
 def before_feature(context, feature):
-    client_to_use = None
-    if 'use_read_api_client' in feature.tags:
-        client_to_use = FlaskTestClient(read_api, DATABASE_NAME)
-    if 'use_write_api_client' in feature.tags:
-        client_to_use = FlaskTestClient(write_api, DATABASE_NAME)
-    if 'use_http_client' in feature.tags:
-        client_to_use = HTTPTestClient(DATABASE_NAME)
-
-    context.client = client_to_use
+    context.client = init_client(feature)
 
 
 def before_scenario(context, _):
@@ -32,6 +24,20 @@ def before_scenario(context, _):
 
 def after_feature(context, _):
     context.client.spin_down()
+
+
+def init_client(feature):
+    if 'use_read_api_client' in feature.tags:
+        return FlaskTestClient(read_api, DATABASE_NAME)
+    if 'use_write_api_client' in feature.tags:
+        return FlaskTestClient(write_api, DATABASE_NAME)
+    if 'use_http_client' in feature.tags:
+        return HTTPTestClient(DATABASE_NAME)
+
+    raise AssertionError(
+        "Test client not selected! Please annotate the failing feature with "
+        + "either @use_read_api_client, @use_write_api_client "
+        + "or @use_http_client.")
 
 
 class FlaskTestClient(object):
