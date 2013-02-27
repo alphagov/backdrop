37abb64ab836951bd038ae461468727866ec615f
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2013-02-27 21:49:42 +0000

    Run jenkins tests with /var/tmp virtualenv.

--- jenkins.sh
+++ jenkins.sh
@@ -2,6 +2,19 @@
 
 set -e
 
+function display_result {
+  RESULT=$1
+  EXIT_STATUS=$2
+  TEST=$3
+
+  if [ $RESULT -ne 0 ]; then
+    echo "$TEST failed"
+    exit $EXIT_STATUS
+  else
+    echo "$TEST passed"
+  fi
+}
+
 VIRTUALENV_DIR=/var/tmp/virtualenvs/$(echo ${JOB_NAME} | tr ' ' '-')
 export PIP_DOWNLOAD_CACHE=/var/tmp/pip_download_cache
 
@@ -11,4 +24,11 @@ source $VIRTUALENV_DIR/bin/activate
 pip install -r requirements.txt
 pip install -r requirements_for_tests.txt
 
-./run_tests.sh
+nosetests -v
+display_result $? 1 "Unit tests"
+
+behave $? 2 "Feature tests"
+display_result $? 2 "Feature tests"
+
+$(dirname $0)/pep-it.sh
+display_result $? 3 "Code style check"
