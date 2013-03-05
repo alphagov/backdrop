1e4a15ff494754ef62dd07100a3560d90ba6c0bd
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2013-03-05 21:54:26 +0000

    Tidy up formatting.

--- performance_platform/read/api.py
+++ performance_platform/read/api.py
@@ -26,12 +26,10 @@ def open_bucket_collection(bucket):
 def validate_request_args(request_args):
     if 'start_at' in request_args:
         if not value_is_valid_datetime_string(request_args['start_at']):
-            return invalid(
-                'start_at is not a valid datetime')
+            return invalid('start_at is not a valid datetime')
     if 'end_at' in request_args:
         if not value_is_valid_datetime_string(request_args['end_at']):
-            return invalid(
-                'end_at is not a valid datetime')
+            return invalid('end_at is not a valid datetime')
     if 'filter_by' in request_args:
         if request_args['filter_by'].find(':') < 0:
             return invalid('filter_by is not valid')
--- performance_platform/write/api.py
+++ performance_platform/write/api.py
@@ -68,24 +68,16 @@ def validate_post_to_bucket(incoming_data, bucket_name):
 def validate_data_object(obj):
     for key, value in obj.items():
         if not key_is_valid(key):
-            return invalid(
-                '{0} is not a valid key'.format(key)
-            )
+            return invalid('{0} is not a valid key'.format(key))
 
         if not value_is_valid(value):
-            return invalid(
-                '{0} is not a valid value'.format(value)
-            )
+            return invalid('{0} is not a valid value'.format(value))
 
         if key == '_timestamp' and not value_is_valid_datetime_string(value):
-            return invalid(
-                '{0} is not a valid timestamp'.format(value)
-            )
+            return invalid('{0} is not a valid timestamp'.format(value))
 
         if key == '_id' and not value_is_valid_id(value):
-            return invalid(
-                '{0} is not a valid _id'.format(value)
-            )
+            return invalid('{0} is not a valid _id'.format(value))
 
     return valid()
 
