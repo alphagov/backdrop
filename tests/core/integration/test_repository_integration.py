99c50fadb05b6f5b0492604c1768522f748f401e
Gareth Rogers <grogers@thoughtworks.com>
2013-03-18 14:47:07 +0000

    Integration tests for repo
    @maxfliri
    @gtrogers

---
+++ tests/core/integration/test_repository_integration.py
@@ -0,0 +1,81 @@
+import unittest
+from hamcrest import *
+from pymongo import MongoClient
+from backdrop.core.repository import Repository
+
+HOST = 'localhost'
+PORT = 27017
+DB_NAME = 'performance_platform_test'
+BUCKET = 'test_repository_integration'
+
+
+class TestRepositoryIntegration(unittest.TestCase):
+    def setUp(self):
+        self.repo = Repository(MongoClient(HOST, PORT)[DB_NAME][BUCKET])
+        self.mongo_collection = MongoClient(HOST, PORT)[DB_NAME][BUCKET]
+
+    def tearDown(self):
+        self.mongo_collection.drop()
+
+    def test_save(self):
+        thing_to_save = {'name': 'test_document'}
+        another_thing_to_save = {'name': '2nd_test_document'}
+
+        self.repo.save(thing_to_save)
+        self.repo.save(another_thing_to_save)
+
+        results = self.mongo_collection.find()
+        assert_that(results, has_item(thing_to_save))
+        assert_that(results, has_item(another_thing_to_save))
+
+    def test_save_updates_document_with_id(self):
+        a_document = { "_id": "event1", "title": "I'm an event"}
+        updated_document = {"_id": "event1", "title": "I'm another event"}
+
+        self.repo.save(a_document)
+        self.repo.save(updated_document)
+
+        saved_documents = self.mongo_collection.find()
+
+        assert_that( saved_documents, only_contains(updated_document) )
+
+    def test_find(self):
+        self.mongo_collection.save({"name": "George", "plays": "guitar"})
+        self.mongo_collection.save({"name": "John", "plays": "guitar"})
+        self.mongo_collection.save({"name": "Paul", "plays": "bass"})
+        self.mongo_collection.save({"name": "Ringo", "plays": "drums"})
+
+        results = self.repo.find({"plays": "guitar"})
+
+        assert_that(results, only_contains(
+            has_entries({"name": "George", "plays": "guitar"}),
+            has_entries({"name": "John", "plays": "guitar"}),
+        ))
+
+    def test_group(self):
+        self.mongo_collection.save({"name": "George", "plays": "guitar"})
+        self.mongo_collection.save({"name": "John", "plays": "guitar"})
+        self.mongo_collection.save({"name": "Paul", "plays": "bass"})
+        self.mongo_collection.save({"name": "Ringo", "plays": "drums"})
+
+        results = self.repo.group("plays", {})
+
+        assert_that(results, only_contains(
+            has_entries({"plays": "guitar", "count": 2}),
+            has_entries({"plays": "bass", "count": 1}),
+            has_entries({"plays": "drums", "count": 1})
+        ))
+
+    def test_group_with_query(self):
+        self.mongo_collection.save({"value": '1', "suite": "hearts"})
+        self.mongo_collection.save({"value": '1', "suite": "diamonds"})
+        self.mongo_collection.save({"value": '1', "suite": "clubs"})
+        self.mongo_collection.save({"value": 'K', "suite": "hearts"})
+        self.mongo_collection.save({"value": 'K', "suite": "diamonds"})
+
+        results = self.repo.group("value", {"suite": "diamonds"})
+
+        assert_that(results, only_contains(
+            {"value": "1", "count": 1},
+            {"value": "K", "count": 1}
+        ))
