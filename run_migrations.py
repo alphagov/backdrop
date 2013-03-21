11350ff9ca61795e7e8d220ade06e855a353f089
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2013-03-21 18:20:17 +0000

    Add a basic migration for adding _week_start_at
    This loads all documents that do not have a _week_start_at field and
    adds it. This is required for the periodic grouping.

---
+++ run_migrations.py
@@ -0,0 +1,42 @@
+"""
+Run all migrations
+"""
+import imp
+import os
+import pymongo
+from os.path import join
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
+
+
+def load_config(env):
+    config_path = os.path.join(ROOT_PATH, 'backdrop', 'write', 'config',
+                               '{0}.py'.format(env))
+    return imp.load_source('config', config_path)
+
+
+def get_database(config):
+    client = pymongo.MongoClient(config.MONGO_HOST, config.MONGO_PORT)
+
+    return client[config.DATABASE_NAME]
+
+
+def get_migrations():
+    migrations_path = join(ROOT_PATH, 'migrations')
+    for migration_file in os.listdir(migrations_path):
+        if migration_file.endswith('.py'):
+            migration_path = join(migrations_path, migration_file)
+
+            yield imp.load_source('migration', migration_path)
+
+
+if __name__ == '__main__':
+
+    config = load_config(os.getenv('GOVUK_ENV', 'development'))
+    database = get_database(config)
+
+    for migration in get_migrations():
+        migration.up(database)
