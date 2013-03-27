7912346e1cb42e4b70a5cd8f9ada5fb9b98e7fe9
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2013-03-27 10:48:12 +0000

    Tee pep8 output so that it's visible in CI console
    @pbadenski
    @robyoung

--- jenkins.sh
+++ jenkins.sh
@@ -31,5 +31,5 @@ python -m coverage.__main__ xml --include=backdrop*
 behave --tags=-wip --stop
 display_result $? 2 "Feature tests"
 
-$(dirname $0)/pep-it.sh > pep8.out
+$(dirname $0)/pep-it.sh | tee pep8.out
 display_result $? 3 "Code style check"
