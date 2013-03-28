e9e76d37563db7c824c1936baad15fa953bd51df
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2013-03-28 13:24:35 +0000

    Remove CI reporting files before building
    Can cause errors if files are deleted. Specifically, coverage will
    attempt to load files that do not exist.

--- jenkins.sh
+++ jenkins.sh
@@ -24,6 +24,8 @@ source $VIRTUALENV_DIR/bin/activate
 pip install -r requirements.txt
 pip install -r requirements_for_tests.txt
 
+rm coverage.xml .coverage nosetests.xml
+
 nosetests -v --with-xunit --with-coverage --cover-package=backdrop --cover-inclusive
 display_result $? 1 "Unit tests"
 python -m coverage.__main__ xml --include=backdrop*
