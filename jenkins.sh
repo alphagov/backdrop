e61b11c805dc252b2a05531e2f4128805b8d83c2
Pawel Badenski <Pawel.Badenski@thoughtworks.com>
2013-04-11 15:22:15 +0100

    Create virtualenv on Jenkins only if doesn't exist
    @robyoung
    @pbadenski

--- jenkins.sh
+++ jenkins.sh
@@ -18,7 +18,9 @@ function display_result {
 VIRTUALENV_DIR=/var/tmp/virtualenvs/$(echo ${JOB_NAME} | tr ' ' '-')
 export PIP_DOWNLOAD_CACHE=/var/tmp/pip_download_cache
 
-virtualenv --clear --no-site-packages $VIRTUALENV_DIR
+if [ ! -e $VIRTUALENV_DIR ]; then
+  virtualenv --no-site-packages $VIRTUALENV_DIR
+fi
 source $VIRTUALENV_DIR/bin/activate
 
 pip install -r requirements.txt
