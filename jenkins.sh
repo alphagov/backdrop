fc952543ee8d03908c2813001b3d23a0eb71ee15
Max Fliri <mfliri@thoughtworks.com>
2013-04-12 10:41:05 +0100

    Revert "Create virtualenv on Jenkins only if doesn't exist"
    This reverts commit c9acc41409be4b9c3959cd78422860d91776c646.

--- jenkins.sh
+++ jenkins.sh
@@ -18,9 +18,7 @@ function display_result {
 VIRTUALENV_DIR=/var/tmp/virtualenvs/$(echo ${JOB_NAME} | tr ' ' '-')
 export PIP_DOWNLOAD_CACHE=/var/tmp/pip_download_cache
 
-if [ ! -e $VIRTUALENV_DIR ]; then
-  virtualenv --no-site-packages $VIRTUALENV_DIR
-fi
+virtualenv --clear --no-site-packages $VIRTUALENV_DIR
 source $VIRTUALENV_DIR/bin/activate
 
 pip install -r requirements.txt
