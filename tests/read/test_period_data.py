2b300efd37a72945e7006df4fe1f4157eb6132b1
Gareth <grogers@thoughtworks.com>
2013-05-03 15:18:38 +0100

    WIP - wrap period data in collection object

--- tests/read/test_period_data.py
+++ tests/read/test_period_data.py
@@ -6,8 +6,14 @@ from tests.support.test_helpers import d, d_tz
 
 
 class PeriodData(object):
+    def __init__(self):
+        self._datum = []
+
     def add(self, document):
-        self.datum = [self.__create_datum(document)]
+        self._datum.append(self.__create_datum(document))
+
+    def data(self):
+        return tuple(self._datum)
 
     def __create_datum(self, doc):
         if doc["_week_start_at"].weekday() is not 0:
@@ -30,11 +36,11 @@ class TestPeriodData(unittest.TestCase):
         period_data = PeriodData()
         period_data.add(stub_doc)
 
-        assert_that(len(period_data.datum), is_(1))
-        assert_that(period_data.datum[0], has_entry("_count", 42))
-        assert_that(period_data.datum[0],
+        assert_that(len(period_data.data()), is_(1))
+        assert_that(period_data.data()[0], has_entry("_count", 42))
+        assert_that(period_data.data()[0],
                     has_entry("_start_at", d_tz(2013, 5, 6)))
-        assert_that(period_data.datum[0],
+        assert_that(period_data.data()[0],
                     has_entry("_end_at", d_tz(2013, 5, 13)))
 
     def test_period_datum_week_start_at_should_be_monday(self):
@@ -51,3 +57,42 @@ class TestPeriodData(unittest.TestCase):
             assert_that(str(e), is_("Weeks MUST start on Monday but got date:"
                                     " " + str(d(2013, 5, 4))))
 
+    def test_adding_more_mongo_documents_to_collection(self):
+        stub_doc = {
+            "_week_start_at": d(2013, 5, 6),
+            "_count": 42
+        }
+        another_stub_doc = {
+            "_week_start_at": d(2013, 5, 13),
+            "_count": 66
+        }
+
+        period_data = PeriodData()
+        period_data.add(stub_doc)
+        period_data.add(another_stub_doc)
+
+        assert_that(len(period_data.data()), is_(2))
+
+        assert_that(period_data.data()[0], has_entry("_start_at",
+                                                    d_tz(2013, 5, 6)))
+        assert_that(period_data.data()[0], has_entry("_end_at",
+                                                    d_tz(2013, 5, 13)))
+
+        assert_that(period_data.data()[1], has_entry("_start_at",
+                                                    d_tz(2013, 5, 13)))
+        assert_that(period_data.data()[1], has_entry("_end_at",
+                                                    d_tz(2013, 5, 20)))
+
+    def test_returned_data_should_be_immutable(self):
+        stub_doc = {
+            "_week_start_at": d(2013, 5, 6),
+            "_count": 42
+        }
+        period_data = PeriodData()
+        period_data.add(stub_doc)
+        the_data = period_data.data()
+        try:
+            the_data.append({"nonsense": True})
+            assert_that(False, "expected an exception")
+        except AttributeError as e:
+            assert_that(str(e), "'tuple' object has no attribute append")
