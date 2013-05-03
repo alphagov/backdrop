dde843f27a7fbab3ded3758fc31ac25408b9002a
Gareth <grogers@thoughtworks.com>
2013-05-03 10:35:24 +0100

    Add test for non _ fields getting serialized correctly

--- tests/read/test_datum.py
+++ tests/read/test_datum.py
@@ -29,7 +29,7 @@ class Datum(object):
 
 class TestDatum(unittest.TestCase):
     def test_datum_should_serialize_to_json(self):
-        stub_doc = {"_id": ObjectId(oid="0"*24)}
+        stub_doc = {"_id": ObjectId(oid="0" * 24)}
         assert_that(
             json.dumps(Datum(stub_doc), cls=Datum.encoder()),
             is_((''
@@ -42,7 +42,7 @@ class TestDatum(unittest.TestCase):
 
     def test_datum_should_serialize_to_json_with_timestamps(self):
         stub_doc = {
-            "_id": ObjectId(oid="0"*24),
+            "_id": ObjectId(oid="0" * 24),
             "_timestamp": d_tz(2013, 4, 1)
         }
         assert_that(
@@ -53,5 +53,30 @@ class TestDatum(unittest.TestCase):
                  '"_timestamp": "2013-04-01T00:00:00+00:00"'
                  '}'
                  ''
+            ))
+        )
+
+    def test_datum_should_serialize_other_fields_to_strings(self):
+        stub_doc = {
+            "_id": ObjectId(oid="0" * 24),
+            "_timestamp": d_tz(2013, 4, 1),
+            "field_1": "foo",
+            "field_2": "bar",
+            "field_3": "zap"
+        }
+
+        json_string = json.dumps(Datum(stub_doc), cls=Datum.encoder())
+
+        assert_that(
+            json.loads(json_string),
+            is_(json.loads(''
+                           '{'
+                           '"_id": "000000000000000000000000", '
+                           '"_timestamp": "2013-04-01T00:00:00+00:00", '
+                           '"field_1": "foo", '
+                           '"field_2": "bar", '
+                           '"field_3": "zap" '
+                           '}'
+                           '',
             ))
         )
