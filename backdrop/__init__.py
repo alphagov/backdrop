0a412ca99b0669a9408941957e943ff1191343ec
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2013-05-13 14:41:29 +0100

    Allow bucket to be provided to statsd.StatClient.
    Prefixes the bucket name to the application part of the stat key.

--- backdrop/__init__.py
+++ backdrop/__init__.py
@@ -4,4 +4,21 @@ import statsd as _statsd
 
 __all__ = ['statsd']
 
-statsd = _statsd.StatsClient(prefix=os.getenv("GOVUK_STATSD_PREFIX"))
+
+class StatsClient(object):
+    """Wrap statsd.StatsClient to allow bucket to be added to stat"""
+    def __init__(self, statsd):
+        self._statsd = statsd
+
+    def __getattr__(self, item):
+        if item in ['timer', 'timing', 'incr', 'decr', 'gauge']:
+            def func(stat, *args, **kwargs):
+                if 'bucket' in kwargs:
+                    stat = '%s.%s' % (kwargs.pop('bucket'), stat)
+                return getattr(self._statsd, item)(stat, *args, **kwargs)
+            return func
+        else:
+            return getattr(self._statsd, item)
+
+statsd = StatsClient(
+    _statsd.StatsClient(prefix=os.getenv("GOVUK_STATSD_PREFIX")))
