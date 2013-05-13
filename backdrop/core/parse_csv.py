ae4c0bb6f1fb65a00db5cc244eb8baa60e44dfbf
Pawel Badenski <Pawel.Badenski@thoughtworks.com>
2013-05-13 16:58:24 +0100

    Num of cols and values in rows should be equal
    Throw exception if number of columns and values in the rows
    is not equal

--- backdrop/core/parse_csv.py
+++ backdrop/core/parse_csv.py
@@ -1,5 +1,23 @@
 import csv
 
 
+class MoreValuesThanColumnsException(Exception):
+    pass
+
+
+class MissingValuesForSomeColumnsException(Exception):
+    pass
+
+
+class DictReader(csv.DictReader):
+    def next(self):
+        next_item = csv.DictReader.next(self)
+        if None in next_item.keys():
+            raise MoreValuesThanColumnsException()
+        if None in next_item.values():
+            raise MissingValuesForSomeColumnsException()
+        return next_item
+
+
 def parse_csv(csv_stream):
-    return csv.DictReader(csv_stream)
+    return DictReader(csv_stream)
--- tests/core/test_csv.py
+++ tests/core/test_csv.py
@@ -1,20 +1,39 @@
 from StringIO import StringIO
+import unittest
 from hamcrest import assert_that, only_contains, is_
-from backdrop.core.parse_csv import parse_csv
+from backdrop.core.parse_csv import parse_csv, MoreValuesThanColumnsException, MissingValuesForSomeColumnsException
 
 
-def test_parse_csv():
-    csv_stream = StringIO("a,b\nx,y\nq,w")
+class ParseCsvTestCase(unittest.TestCase):
+    def test_parse_csv(self):
+        csv_stream = StringIO("a,b\nx,y\nq,w")
 
-    data = list(parse_csv(csv_stream))
+        data = list(parse_csv(csv_stream))
 
-    assert_that(data,
-                only_contains({"a": "x", "b": "y"}, {"a": "q", "b": "w"}))
+        assert_that(data,
+                    only_contains({"a": "x", "b": "y"}, {"a": "q", "b": "w"}))
 
+    def test_parse_csv_when_values_for_columns_missing(self):
+        csv_stream = StringIO("a,b\nx,y\nq")
 
-def test_parse_empty_csv():
-    csv_stream = StringIO("")
+        try:
+            list(parse_csv(csv_stream))
+            assert_that(False)
+        except MissingValuesForSomeColumnsException as e:
+            pass
 
-    data = list(parse_csv(csv_stream))
+    def test_parse_csv_when_more_values_than_columns(self):
+        csv_stream = StringIO("a,b\nx,y,s,d\nq,w")
 
-    assert_that(data, is_([]))
+        try:
+            list(parse_csv(csv_stream))
+            assert_that(False)
+        except MoreValuesThanColumnsException as e:
+            pass
+
+    def test_parse_empty_csv(self):
+        csv_stream = StringIO("")
+
+        data = list(parse_csv(csv_stream))
+
+        assert_that(data, is_([]))
