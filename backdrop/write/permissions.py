62474bf3ca7faac96662a296bfca28f30b223836
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2013-06-04 12:28:18 +0100

    Add unit tests for Permissions class
    @maxfliri, @alexmuller

--- backdrop/write/permissions.py
+++ backdrop/write/permissions.py
@@ -3,4 +3,4 @@ class Permissions(object):
         self._permissions = permissions
 
     def is_user_allowed_to_bucket(self, user, bucket_name):
-        return user in self._permissions[bucket_name]
+        return user in self._permissions.get(bucket_name, [])
