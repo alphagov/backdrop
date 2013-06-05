2b11e9431ea6b3a816cd99ac7b42906a4b83fb38
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2013-06-05 13:49:37 +0100

    Add trailing comma
    To make diffs easier.

--- backdrop/write/config/development_tokens.py
+++ backdrop/write/config/development_tokens.py
@@ -10,5 +10,5 @@ TOKENS = {
     'pay_register_birth_abroad_journey': 'pay_register_birth_abroad_journey-bearer-token',
     'pay_register_death_abroad_journey': 'pay_register_death_abroad_journey-bearer-token',
     'pay_foreign_marriage_certificates_journey': 'pay_foreign_marriage_certificates_journey-bearer-token',
-    'deposit_foreign_marriage_journey': 'deposit_foreign_marriage_journey-bearer-token'
+    'deposit_foreign_marriage_journey': 'deposit_foreign_marriage_journey-bearer-token',
 }
