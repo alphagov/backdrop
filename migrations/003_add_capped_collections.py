4f7be8d8f3b811e296ee84d2aea033d029a46c01
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2013-06-11 14:33:05 +0000

    Re-add fco realtime collections.

--- migrations/003_add_capped_collections.py
+++ migrations/003_add_capped_collections.py
@@ -8,9 +8,15 @@ log = logging.getLogger(__name__)
 
 def up(db):
     capped_collections = [
+        "fco_realtime_pay_legalisation_post",
+        "fco_realtime_pay_legalisation_drop_off",
+        "fco_realtime_pay_register_birth_abroad",
+        "fco_realtime_pay_register_death_abroad",
+        "fco_realtime_pay_foreign_marriage_certificates",
+        "fco_realtime_deposit_foreign_marriage",
         "govuk_realtime",
-        "licensing_realtime"
-    ]
+        "licensing_realtime",
+   ]
 
     for collection_name in capped_collections:
         db.create_collection(name=collection_name, capped=True, size=5040)
