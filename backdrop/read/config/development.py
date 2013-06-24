e1de7d3e661f5df43ee77ea59bc8fcd4934fafeb
Max Fliri <mfliri@thoughtworks.com>
2013-06-24 15:54:24 +0100

    Development configuration for lpa volumes bucket

--- backdrop/read/config/development.py
+++ backdrop/read/config/development.py
@@ -11,4 +11,5 @@ RAW_QUERIES_ALLOWED = {
     "pay_register_death_abroad_journey": True,
     "pay_foreign_marriage_certificates_journey": True,
     "deposit_foreign_marriage_journey": True,
+    "lpa_volumes": True,
 }
--- backdrop/write/config/development.py
+++ backdrop/write/config/development.py
@@ -5,6 +5,9 @@ LOG_LEVEL = "DEBUG"
 SINGLE_SIGN_ON = True
 SECRET_KEY = "something unique and secret"
 BACKDROP_ADMIN_UI_HOST = "http://backdrop-admin.dev.gov.uk"
+BUCKET_AUTO_ID = {
+    "lpa_volumes": ("key", "start_at", "end_at")
+}
 
 try:
     from development_environment import *
