e8991c80af01518e61168912e236438496ed3265
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2013-07-04 13:59:27 +0100

    Refactor test running into one source of truth
    - In development run `./run_tests.sh`
    - `jenkins.sh` uses `./run_tests.sh`
    - `.travis` uses `./run_tests.sh`
    
    Simples!
    
    This assumes that you're using virtualenvwrapper in development (or at
    least have your `WORKON_HOME` environment variable set). If you're not
    then you will need to be in a virtualenv before executing
    `./run_tests.sh`

--- run_tests.sh
+++ run_tests.sh
@@ -1,39 +1,41 @@
 #!/bin/bash
 
 function display_result {
-    RESULT=$1
-    EXIT_STATUS=$2
-    TEST=$3
+  RESULT=$1
+  EXIT_STATUS=$2
+  TEST=$3
 
-    if [ $RESULT -ne 0 ]; then
-      echo
-      echo -e "\033[31m$TEST failed\033[0m"
-      echo
-      exit $EXIT_STATUS
-    else
-      echo
-      echo -e "\033[32m$TEST passed\033[0m"
-      echo
-    fi
+  if [ $RESULT -ne 0 ]; then
+    echo -e "\033[31m$TEST failed\033[0m"
+    exit $EXIT_STATUS
+  else
+    echo -e "\033[32m$TEST passed\033[0m"
+  fi
 }
-basedir=$(dirname $0)
-venvdir=~/.virtualenvs/$(basename $(cd $(dirname $0) && pwd -P))
 
-if [ ! -d $venvdir ]; then
-  virtualenv $venvdir
+# If you're not already in a virtualenv and you're using virtualenvwrapper
+if [ -z "$VIRTUAL_ENV" -a -n "$WORKON_HOME" ]; then
+  basedir=$(dirname $0)
+  venvdir=$WORKON_HOME/$(basename $(cd $(dirname $0) && pwd -P))
+
+  if [ ! -d "$venvdir" ]; then
+    virtualenv $venvdir
+  fi
+
+  source "$venvdir/bin/activate"
 fi
 
-source "$venvdir/bin/activate"
 
 pip install -r requirements_for_tests.txt
 
+rm -f coverage.xml .coverage nosetests.xml
 find . -name '*.pyc' -delete
 
-nosetests -v
+nosetests -v --with-xunit --with-coverage --cover-package=backdrop --cover-inclusive
 display_result $? 1 "Unit tests"
 
 behave --stop --tags=-wip
 display_result $? 2 "Feature tests"
 
-"$basedir/pep-it.sh"
+./pep-it.sh | tee pep8.out
 display_result $? 3 "Code style check"
