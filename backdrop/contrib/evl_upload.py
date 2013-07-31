5a48305f412ae17dc9a21b129a2b7d95fd6a02c8
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2013-07-31 09:08:39 +0100

    Move EVL CEG functions inside main parse function
    Make the fact they're only for EVL CEG parsing explicit.

--- backdrop/contrib/evl_upload.py
+++ backdrop/contrib/evl_upload.py
@@ -1,38 +1,41 @@
-from datetime import timedelta, datetime
+from datetime import datetime
 import itertools
 from tests.support.test_helpers import d_tz
 
 
 def ceg_volumes(rows):
-    yield _ceg_keys(rows)
+    def ceg_keys(rows):
+        return [
+            "_timestamp", "timeSpan", "relicensing_web", "relicensing_ivr",
+            "relicensing_agent", "sorn_web", "sorn_ivr", "sorn_agent",
+            "agent_automated_dupes", "calls_answered_by_advisor"
+        ]
 
-    for row in _ceg_rows(rows):
+    def ceg_rows(rows):
+        for column in itertools.count(3):
+            date = ceg_date(rows, column)
+            if not isinstance(date, datetime):
+                return
+            if date >= d_tz(2012, 4, 1):
+                yield [
+                    date, "month", rows[5][column], rows[6][column],
+                    rows[9][column], rows[11][column], rows[12][column],
+                    rows[13][column], rows[15][column], rows[17][column]
+                ]
+
+    def ceg_date(rows, column):
+        try:
+            return rows[3][column]
+        except IndexError:
+            return None
+
+    yield ceg_keys(rows)
+
+    for row in ceg_rows(rows):
         yield row
 
 
-def _ceg_keys(rows):
-    return [
-        "_timestamp", "timeSpan", "relicensing_web", "relicensing_ivr",
-        "relicensing_agent", "sorn_web", "sorn_ivr", "sorn_agent",
-        "agent_automated_dupes", "calls_answered_by_advisor"
-    ]
 
 
-def _ceg_rows(rows):
-    for column in itertools.count(3):
-        date = _ceg_date(rows, column)
-        if not isinstance(date, datetime):
-            return
-        if date >= d_tz(2012, 4, 1):
-            yield [
-                date, "month", rows[5][column], rows[6][column],
-                rows[9][column], rows[11][column], rows[12][column],
-                rows[13][column], rows[15][column], rows[17][column]
-            ]
 
 
-def _ceg_date(rows, column):
-    try:
-        return rows[3][column]
-    except IndexError:
-        return None
