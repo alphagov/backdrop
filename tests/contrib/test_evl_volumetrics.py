b39d4534d79bbfb003fa368b86e4c1f41790e243
Paulo Schneider <paulo.schneider@gmail.com>
2013-08-27 15:21:25 +0100

    Remove summary columns

--- tests/contrib/test_evl_volumetrics.py
+++ tests/contrib/test_evl_volumetrics.py
@@ -1,12 +1,18 @@
+from itertools import ifilter
 from pprint import pprint
+import re
 import unittest
 from hamcrest import *
 from datetime import datetime
 from backdrop.core.timeutils import as_utc
 
 
-def extract_transaction_rows(sheet):
+def extract_column_header(sheet):
     HEADER_INDEX = 3
+    return sheet[HEADER_INDEX]
+
+
+def extract_transaction_rows(sheet):
     TRANSACTION_INDEXES = {
            4: ["Assisted Digital", "Relicensing"],
            5: ["Assisted Digital", "Relicensing"],
@@ -35,7 +41,7 @@ def extract_transaction_rows(sheet):
         channel_service = TRANSACTION_INDEXES[index]
         return channel_service + sheet[index][2:4]
 
-    return sheet[HEADER_INDEX], map(transaction_row, TRANSACTION_INDEXES.keys())
+    return extract_column_header(sheet), map(transaction_row, TRANSACTION_INDEXES.keys())
 
 
 def create_transaction_data(header, row):
@@ -60,6 +66,29 @@ def create_transaction_data(header, row):
         return [date.isoformat(), service, channel, transaction, volume]
 
     return map(transaction_data, volumes)
+
+
+def remove_summary_columns(sheet):
+    DATES_START_INDEX = 3
+    DATE_REGEXP = re.compile("[A-Z][a-z]{2}\s\d{4}")
+
+    header = extract_column_header(sheet)
+
+    def add_date_index(mem, i):
+        if bool(DATE_REGEXP.match(header[i])):
+            mem.append(i)
+            return mem
+        else:
+            return mem
+
+    date_indexes = reduce(add_date_index, range(DATES_START_INDEX,len(header)), [])
+
+    def remove_columns_from_row(row):
+        return row[:DATES_START_INDEX] + [row[i] for i in date_indexes]
+
+
+    return map(remove_columns_from_row, sheet)
+
 
 
 class TestEVLVolumetrics(unittest.TestCase):
@@ -131,8 +160,50 @@ class TestEVLVolumetrics(unittest.TestCase):
            ["Manual", "SORN", "V-V890 SORN Declaration Vehicles Triage", 1027]
         ))
 
-    def test_extracting_header_removes_summary(self):
-        pass
+    def test_removes_summary_columns(self):
+        sheet = [
+           ["_", "_", "_", "_", "_", "_"],
+           ["_", "_", "_", "_", "_", "_"],
+           ["_", "_", "_", "_", "_", "_"],
+           ["Channel Descriptions", "", "Transaction", "Apr 2012", "2012/13 Total", "Mar 2013"],
+           ["Assisted Digital", "Relicensing", "V-V10 Licence Application Post Office", 1000, 2000, 3000],
+           ["", "", "V-V11 Licence Renewal Reminder Post Office", 1001, 2001, 3001,],
+           ["_", "_", "_", "_", "_", "_"],
+           ["", "SORN", "V-V11 Some transaction", 1003, 2003, 3003],
+           ["_", "_", "_", "_", "_", "_"],
+           ["_", "_", "_", "_", "_", "_"],
+           ["Fully Digital", "Relicensing", "V-V10 Licence Application EVL", 1006, 2006, 3006],
+           ["", "", "V-V11 Fleets", 1007, 2007, 3007],
+           ["", "", "V-V11 Licence Renewal Reminder EVL", 1008, 2008, 3008],
+           ["", "", "V-V85 and V85/1 HGV Licence Application EVL", 1009, 2008, 3008],
+           ["_", "_", "_", "_", "_", "_"],
+           ["", "SORN", "V-V11 SORN EVL", 1011, 2011, 3011],
+           ["", "", "V-V85/1 HGV SORN Declaration EVL", 1012, 2012, 3012],
+           ["", "", "V-V890 SORN Declaration EVL", 1013, 2013, 3013],
+           ["", "", "V-V890 SORN Declaration Fleets", 1014, 2014, 3014],
+           ["_", "_", "_", "_", "_", "_"],
+           ["_", "_", "_", "_", "_", "_"],
+           ["Manual", "Relicensing", "V-V890 Another transaction", 1017, 2017, 3017],
+           ["", "", "V-V11 Licence Renewal Reminder Local Office", 1018, 2018, 3018],
+           ["", "", "V-V85 and V85/1 HGV Licence Application", 1019, 2019, 3019],
+           ["_", "_", "_", "_", "_", "_"],
+           ["", "SORN", "V-V11 SORN Local Office", 1021, 2021, 3021],
+           ["", "", "V-V85/1 HGV SORN Declaration", 1022, 2022, 3022],
+           ["", "", "V-V890 SORN Declaration", 1023, 2023, 3023],
+           ["", "", "V-V890 SORN Declaration Key from Image", 1024, 2024, 3024],
+           ["", "", "V-V890 SORN Declaration Refunds Input", 1025, 2025, 3025],
+           ["", "", "V-V890 SORN Declaration Vehicles Input", 1026, 2026, 3026],
+           ["", "", "V-V890 SORN Declaration Vehicles Triage", 1027, 2027, 3027],
+           ["_", "_", "_", "_", "_", "_"],
+           ["_", "_", "_", "_", "_", "_"],
+           ["_", "_", "_", "_", "_", "_"],
+        ]
+
+        assert_that(remove_summary_columns(sheet), has_items(
+            ["Channel Descriptions", "", "Transaction", "Apr 2012", "Mar 2013"],
+            ["", "", "V-V85 and V85/1 HGV Licence Application EVL", 1009, 3008],
+        ))
+
 
     def test_creating_transaction_data_from_row_and_header(self):
         header = ["Channel Descriptions", "", "Transaction", "Apr 2012", "Mar 2013", "May 2013"]
