fe3e5cb34b4ee961e4a57ae0cc4e03adae97c7b0
Paulo Schneider <paulo.schneider@gmail.com>
2013-09-13 16:01:10 +0100

    Integration test for saving a bucket config

--- tests/core/integration/test_bucket_repository.py
+++ tests/core/integration/test_bucket_repository.py
@@ -1,1 +1,35 @@
-__author__ = 'pschneid'
+import unittest
+from hamcrest import assert_that, is_, has_entries
+from backdrop.core.bucket import BucketConfig
+from backdrop.core.database import MongoDriver
+from pymongo import MongoClient
+from backdrop.core.repository import BucketRepository
+
+HOST = 'localhost'
+PORT = 27017
+DB_NAME = 'performance_platform_test'
+BUCKET = 'buckets_config_test'
+
+
+class TestBucketRepositoryIntegration(unittest.TestCase):
+
+    def setUp(self):
+        mongo_driver = MongoDriver(MongoClient(HOST, PORT)[DB_NAME][BUCKET])
+        mongo_driver._collection.drop()
+        self.mongo_collection = MongoClient(HOST, PORT)[DB_NAME][BUCKET]
+        self.repository = BucketRepository(mongo_driver)
+
+    def test_saving_a_config_with_default_values(self):
+        config = BucketConfig("some_bucket")
+
+        self.repository.save(config)
+
+        results = list(self.mongo_collection.find())
+
+        assert_that(len(results), is_(1))
+        assert_that(results[0], has_entries({
+            "name": "some_bucket",
+            "raw_queries_allowed": False,
+            "bearer_token": None,
+            "upload_format": "csv"
+        }))
