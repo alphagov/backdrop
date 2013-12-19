6d64efbcc23f1fd16da3d09da25eb17f90f7e08e
Nick Gravgaard <nick@beautifulcode.net>
2013-12-19 16:51:42 +0000

    skip features tagged use_splinter_client on Travis
    Travis has problems running feature tests even though they run fine on our dev boxes, so we want to skip them for now

--- run_tests.sh
+++ run_tests.sh
@@ -41,7 +41,13 @@ display_result $? 1 "Unit tests"
 python -m coverage.__main__ xml --include=backdrop*
 
 # run feature tests
-behave --stop --tags=~@wip
+if [[ -z "$SKIP_SPLINTER_TESTS" ]]; then
+  echo "Not skipping splinter tests"
+  behave --stop --tags=~@wip
+else
+  echo "Skipping splinter tests"
+  behave --stop --tags=~@wip --tags=~@use_splinter_client
+fi
 display_result $? 2 "Feature tests"
 
 # run style checks
