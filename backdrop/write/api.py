f1e84cbc99ff77da88a78ead82d5ac0ad1cf603d
Paul M Furley <paul@paulfurley.com>
2014-02-10 14:05:45 +0000

    Implement the "create collection" endpoint

--- backdrop/write/api.py
+++ backdrop/write/api.py
@@ -1,4 +1,5 @@
 from os import getenv
+import json
 
 from flask import Flask, request, jsonify, g
 from flask_featureflags import FeatureFlag
@@ -11,7 +12,7 @@ from backdrop.core.repository import (BucketConfigRepository,
 from ..core.errors import ParseError, ValidationError
 from ..core import database, log_handler, cache_control
 
-from .validation import bearer_token_is_valid
+from .validation import bearer_token_is_valid, extract_bearer_token
 
 
 GOVUK_ENV = getenv("GOVUK_ENV", "development")
@@ -81,6 +82,46 @@ def write_by_group(data_group, data_type):
 def post_to_bucket(bucket_name):
     bucket_config = bucket_repository.retrieve(name=bucket_name)
     return _write_to_bucket(bucket_config)
+
+
+@app.route('/data-sets/<dataset_name>', methods=['POST'])
+@cache_control.nocache
+def create_collection_for_dataset(dataset_name):
+    if not _allow_create_collection(request.headers.get('Authorization')):
+        return jsonify(status='error',
+                       message="Forbidden: invalid or no token given."), 403
+
+    if db.collection_exists(dataset_name):
+        return jsonify(status='error',
+                       message='Collection exists with that name.'), 400
+
+    try:
+        data = json.loads(request.data)
+    except ValueError as e:
+        return jsonify(status='error', message=repr(e)), 400
+    else:
+        capped_size = data.get('capped_size', None)
+
+    if capped_size is None or not isinstance(capped_size, int):
+        return jsonify(
+            status='error',
+            message="You must specify an int capped_size of 0 or more"), 400
+
+    if capped_size == 0:
+        db.create_uncapped_collection(dataset_name)
+    else:
+        db.create_capped_collection(dataset_name, capped_size)
+
+    return jsonify(status='ok', message='Created "{}"'.format(dataset_name))
+
+
+def _allow_create_collection(auth_header):
+    token = extract_bearer_token(auth_header)
+    if token == app.config['CREATE_COLLECTION_ENDPOINT_TOKEN']:
+        return True
+
+    app.logger.info("Bad token for create collection: '{}'".format(token))
+    return False
 
 
 def _write_to_bucket(bucket_config):
