1fa2396bef908b9f67be9f79576c99df580fd7fd
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2014-04-09 20:51:23 +0100

    Generate shorter tokens
    Following on from @paulfurley's great change to /dev/urandom in
    b4eafedfaa4e156a86ae1d72831ca350ee951b2e, I've implemented @robyoung's
    comment on #262 to shorten the token length to 64 characters.
    
    I've also increased the characters included from a-f0-9 to a-zA-Z0-9.
    
    I don't really want to make big assertions about cryptography, but
    this has reduced the key space from 10^154 to 10^114, so I think
    we're probably still ok.

--- tools/generate-token.sh
+++ tools/generate-token.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 #
-# Generate a random token 
+# Generate a random token
 #
 
-head -c 500 /dev/urandom | shasum -a 512
+# LC_CTYPE=C is specified for OS X, as otherwise tr will return
+# an illegal byte sequence from assuming /dev/urandom is UTF-8
+
+cat /dev/urandom | LC_CTYPE=C tr -cd 'a-zA-Z0-9' | head -c 64
+
+echo '' # Add a newline
