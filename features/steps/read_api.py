c2f574f320419c01cb0a6f91de631492add91592
Ralph Cowling <ralph.cowling@digital.cabinet-office.gov.uk>
2014-04-24 18:03:10 +0100

    Add in api_common for shared helper functions

--- features/steps/read_api.py
+++ features/steps/read_api.py
@@ -7,7 +7,9 @@ from dateutil import parser
 import datetime
 import re
 import pytz
+from api_common import ensure_data_set_exists
 from features.support.stagecraft import StagecraftService
+
 
 FIXTURE_PATH = os.path.join(os.path.dirname(__file__), '..', 'fixtures')
 TEST_STAGECRAFT_PORT = 3204
@@ -16,50 +18,6 @@ TEST_STAGECRAFT_PORT = 3204
 @given('the api is running in protected mode')
 def step(context):
     context.client.set_config_parameter('PREVENT_RAW_QUERIES', True)
-
-
-def ensure_data_set_exists(context, data_set_name, settings={}):
-    # these should mostly match the default DataSetConfig.__new__() kwargs
-    response = {
-        'name': data_set_name,
-        'data_group': data_set_name,
-        'data_type': data_set_name,
-        'raw_queries_allowed': False,
-        'bearer_token': '%s-bearer-token' % data_set_name,
-        'upload_format': 'csv',
-        'upload_filters': ['backdrop.core.upload.filters.first_sheet_filter'],
-        'auto_ids': None,
-        'queryable': True,
-        'realtime': False,
-        'capped_size': 5040,
-        'max_age_expected': 2678400,
-    }
-
-    response.update(settings)
-
-    url_response_dict = {
-        ('GET', u'data-sets/{}'.format(data_set_name)): response,
-        ('GET', u'data-sets'): [response],
-        ('GET', u'data-sets?data-group={}&data-type={}'.format(
-            response['data_group'], response['data_type'])): [response],
-    }
-
-    if 'mock_stagecraft_server' in context and context.mock_stagecraft_server:
-        context.mock_stagecraft_server.stop()
-    context.mock_stagecraft_server = StagecraftService(
-        TEST_STAGECRAFT_PORT, url_response_dict)
-    context.mock_stagecraft_server.start()
-
-    context.data_set = data_set_name
-    data_set_data = {
-        '_id': data_set_name,
-        'name': data_set_name,
-        'data_group': data_set_name,
-        'data_type': data_set_name,
-        'bearer_token': "%s-bearer-token" % data_set_name
-    }
-    context.client.storage()["data_sets"].save(data_set_data)
-
 
 @given('"{fixture_name}" is in "{data_set_name}" data_set')
 def step(context, fixture_name, data_set_name):
