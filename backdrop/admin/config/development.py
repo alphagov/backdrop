a636266ed2aed3753a0b28796734aeac7f59fc2a
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2014-05-09 11:55:36 +0100

    Fix dev stagecraft data-set token
    It did not match the development token in stagecraft.

--- backdrop/admin/config/development.py
+++ backdrop/admin/config/development.py
@@ -13,4 +13,4 @@ except ImportError:
     from development_environment_sample import *
 
 STAGECRAFT_URL = 'http://localhost:3204'
-STAGECRAFT_DATA_SET_QUERY_TOKEN = 'stagecraft-data-set-query-token-fake'
+STAGECRAFT_DATA_SET_QUERY_TOKEN = 'dev-data-set-query-token'
