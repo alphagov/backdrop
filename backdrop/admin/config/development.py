143f6762c99c927e09625a67c3e87e9315708ae2
Ralph Cowling <ralph.cowling@digital.cabinet-office.gov.uk>
2014-07-30 12:50:07 +0100

    -

--- backdrop/admin/config/development.py
+++ backdrop/admin/config/development.py
@@ -7,7 +7,6 @@ SECRET_KEY = "something unique and secret"
 
 wercker_mongo_host = os.environ.get('WERCKER_MONGODB_HOST')
 
-
 DATABASE_NAME = "backdrop"
 MONGO_HOSTS = [wercker_mongo_host if wercker_mongo_host else 'localhost']
 MONGO_PORT = 27017
