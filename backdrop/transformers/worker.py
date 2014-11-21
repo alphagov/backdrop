15b10988061e64566cfce44b926ee8c5f6740522
Matt Cottingham <matt.cottingham@digital.cabinet-office.gov.uk>
2014-11-21 16:21:39 +0000

    Add rabbitmq test config

--- backdrop/transformers/worker.py
+++ backdrop/transformers/worker.py
@@ -3,6 +3,7 @@ from celery import Celery
 # Load the appropriate config file as a python module
 import importlib
 from os import getenv
+
 GOVUK_ENV = getenv("GOVUK_ENV", "development")
 config = importlib.import_module("backdrop.write.config.{}".format(GOVUK_ENV))
 
@@ -10,6 +11,7 @@ app = Celery(
     'transformations',
     broker=config.TRANSFORMER_AMQP_URL,
     include=['backdrop.transformers.tasks'])
+
 
 if __name__ == '__main__':
     app.start()
