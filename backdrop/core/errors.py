afbc8f2ce60426f01c0161e092963af9bbe7d8a4
Matt Cottingham <matt.cottingham@digital.cabinet-office.gov.uk>
2015-02-03 16:14:47 +0000

    Submit metrics for transform tasks
    This submits coarse statsd metrics for transform tasks:
    * Counter for task dispatches
    * Timer for running tasks
    * Task error counter.

--- backdrop/core/errors.py
+++ backdrop/core/errors.py
@@ -1,3 +1,6 @@
+from functools import wraps
+
+
 class BackdropError(StandardError):
     pass
 
@@ -23,3 +26,21 @@ class InvalidOperationError(ValueError):
     """Raised if an invalid collect function is provided, or if an error
     is raised from a collect function"""
     pass
+
+
+def incr_on_error(stats_client, metric_name):
+    """
+    Increments metric_name if func() raises an exception, then
+    re-raises the exception.
+    """
+
+    def decorator(func):
+        @wraps(func)
+        def wrapped(*args, **kwargs):
+            try:
+                func(*args, **kwargs)
+            except:
+                stats_client.incr(metric_name)
+                raise
+        return wrapped
+    return decorator
