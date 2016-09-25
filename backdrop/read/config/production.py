64031af129f78ca945c9146334055fe6ad08d6e7
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2016-09-25 07:41:47 +0100

    Add production config
    This is copied from alphagov-deployment. These files don't contain
    any secrets, but they import from a file called `environment.py`
    which does.

---
+++ backdrop/read/config/production.py
@@ -0,0 +1,8 @@
+import os
+
+DATABASE_NAME = "backdrop"
+MONGO_HOSTS = ['performance-mongo-1', 'performance-mongo-2', 'performance-mongo-3']
+MONGO_PORT = 27017
+LOG_LEVEL = "INFO"
+
+from environment import *
---
+++ backdrop/write/config/production.py
@@ -0,0 +1,7 @@
+DATABASE_NAME = "backdrop"
+MONGO_HOSTS = ['performance-mongo-1', 'performance-mongo-2', 'performance-mongo-3']
+MONGO_PORT = 27017
+LOG_LEVEL = "INFO"
+SESSION_COOKIE_SECURE=True
+
+from environment import *
