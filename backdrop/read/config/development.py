735d396b2830f73500a943426e2c0bffe44ac4e1
ambrozic <ambrozic@gmail.com>
2017-06-09 16:43:02 +0100

    added development_environment

--- backdrop/read/config/development.py
+++ backdrop/read/config/development.py
@@ -6,3 +6,8 @@ LOG_LEVEL = "DEBUG"
 STAGECRAFT_URL = 'http://localhost:3103'
 
 SIGNON_API_USER_TOKEN = 'development-oauth-access-token'
+
+try:
+    from .development_environment import *
+except ImportError:
+    pass
