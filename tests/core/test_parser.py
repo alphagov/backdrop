2de8705ca956ac9ad52049d899688cb4e30fd5e8
ambrozic <ambrozic@gmail.com>
2017-06-27 16:09:54 +0100

    Unicode parsing support
    Added support for parsing unicode characters in data

--- tests/core/test_parser.py
+++ tests/core/test_parser.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from unittest import TestCase
 
 from backdrop.core import parser
@@ -23,4 +24,12 @@ class TestParser(TestCase):
             dict(a="1", bb=22, c=3, cc=33),
         ]
         csv = "a,b,bb,c,cc\r\n1,2,,3,\r\n1,2,,,33\r\n1,,22,3,33\r\n"
+        assert csv == parser.json_to_csv(data=data)
+
+    def test_unicode(self):
+        data = [
+            dict(a=u"😇", b=2, c=1),
+            dict(a=u"😈", b=22, c=11),
+        ]
+        csv = "a,b,c\r\n😇,2,1\r\n😈,22,11\r\n"
         assert csv == parser.json_to_csv(data=data)
