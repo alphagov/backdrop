35768cda2f9c5a40931dbbae1e6219aae242737f
Lee Longmore <lee.longmore@gmail.com>
2017-11-14 11:14:31 +0000

    Add missing path to urls

--- backdrop/transformers/config/production.py
+++ backdrop/transformers/config/production.py
@@ -6,5 +6,5 @@ BROKER_URL = PAAS.get('REDIS_URL') or os.getenv('REDIS_URL')
 BROKER_FAILOVER_STRATEGY = "round-robin"
 STAGECRAFT_URL = 'https://performance-platform-stagecraft-production.cloudapps.digital'
 STAGECRAFT_OAUTH_TOKEN = os.getenv('STAGECRAFT_OAUTH_TOKEN')
-BACKDROP_READ_URL = 'https://performance-platform-backdrop-read-production.cloudapps.digital'
-BACKDROP_WRITE_URL = 'https://performance-platform-backdrop-write-production.cloudapps.digital'
+BACKDROP_READ_URL = 'https://performance-platform-backdrop-read-production.cloudapps.digital/data'
+BACKDROP_WRITE_URL = 'https://performance-platform-backdrop-write-production.cloudapps.digital/data'
